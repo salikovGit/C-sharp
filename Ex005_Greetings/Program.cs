ef9a57640913c3d35792751705f7d419dcf9e745
salikov <iesalikov@gmail.com>
2022-08-01 23:22:16 +0500

    Greetings

---
+++ Ex005_Greetings/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Greetings, Masha!");
+}
+else{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
